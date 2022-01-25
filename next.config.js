3353eecd2a15a7017c3effdf4a65c54073ad78a0
Pac <public@pa0x43.me>
2022-01-26 00:41:47 +0100

    Added Env Vars to Runtime Config

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
     config.experiments = {topLevelAwait: true}
     return config
   },
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_GO_REVISION: process.env.NEXT_PUBLIC_GO_REVISION,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+  }
 }
