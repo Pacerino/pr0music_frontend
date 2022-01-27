f18c75be2c42443ba22c505041c27cc8f1d04b9e
Pac <public@pa0x43.me>
2022-01-28 00:51:23 +0100

    Navbar Refactor

--- components/navbar.js
+++ components/navbar.js
@@ -17,8 +17,9 @@ import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Tooltip from '@mui/material/Tooltip';
-import { withStyles } from '@mui/styles';
-import { getSession } from "next-auth/react"
+import { createStyles, makeStyles } from '@mui/styles';
+import { useSession } from "next-auth/react"
+import Skeleton from '@mui/material/Skeleton';
 
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from "@mui/icons-material/AccountCircle";
@@ -30,7 +31,7 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import QueryStatsIcon from '@mui/icons-material/QueryStats';
 
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => createStyles({
   root: {
     position: "absolute",
     right: 0
@@ -45,178 +46,169 @@ const styles = (theme) => ({
       display: "none"
     },
   }
-});
+}));
 
+const Navbar = () => {
+    const { data: session, status } = useSession()
+    const loading = status === "loading"
+    const classes = useStyles();
 
-const Navbar = class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      achorEL: null,
-      drawerOpen: false,
-      session: {}
+    const [drawer, setDrawer] = React.useState(false)
+    const toggleDrawer = (open) => (event) => {
+      if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+      }
+  
+      setDrawer(open)
     };
-  }
-
-  async componentDidMount() {
-    const session = await getSession()
-    if(session != null) {
-      this.setState({session: session})
-    }
-  }
-
-  render() {
-    const handleMenu = (event) => {
-      this.setState({ anchorEL: event.currentTarget });
+    const [dropdown, setDropdown] = React.useState(null)
+    const dropdownOpen = Boolean(dropdown);
+    const toggleDropdown = (event) => {
+      if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+      }
+  
+      setDropdown(event.currentTarget)
     };
-
-    const handleClose = () => {
-      this.setState({ anchorEL: null });
-    };
-
-    const handleDrawerClose = () => {
-      this.setState({drawerOpen: false})
-    }
-
-    const handleDrawer = () => {
-      this.setState({ drawerOpen: true })
-    }
-    return (
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar variant="dense">
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            className={this.props.classes.drawerButton}
-            onClick={handleDrawer}
-          >
-            <MenuIcon />
-          </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Button color="inherit" href="/">PR0GRAMM_MUSIC</Button>
-            </Typography>
-            <ButtonGroup variant="text" className={this.props.classes.buttonBar}>
-              {this.state.session.roles?.includes('admin') && <Button color="inherit" startIcon={<ArticleOutlinedIcon />} href="/admin">Admin Dashboard</Button>}
-              <Button color="inherit" startIcon={<ArticleOutlinedIcon />} href="/changelog">Changelog</Button>
-              <Button color="inherit" startIcon={<QueryStatsIcon />} href="/stats">Statistik</Button>
-              <Button color="inherit" startIcon={<DescriptionIcon />} href="https://doc.pr0sauce.info">API Docs</Button>
-              <Button color="inherit" startIcon={<GitHubIcon />} href="https://github.com/pacerino">GitHub</Button>
-              {!this.state?.session.user ? (
-                <Button color="inherit" onClick={signIn}>
-                  Login
-                </Button>
-              ) : (
-                  <Button
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleMenu}
-                    color="inherit"
-                    endIcon={<AccountCircle />}
-                  >
-                    {this.state.session.user.name}
+    if(loading) {
+      return <Skeleton animation="wave" variant="rectangular" height={50} />
+    } else {
+      return (
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar variant="dense">
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+              className={classes.drawerButton}
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Button color="inherit" href="/">PR0GRAMM_MUSIC</Button>
+              </Typography>
+              <ButtonGroup variant="text" className={classes.buttonBar}>
+                {session?.user.roles.includes('admin') && <Button color="inherit" startIcon={<ArticleOutlinedIcon />} href="/admin">Admin Dashboard</Button>}
+                <Button color="inherit" startIcon={<ArticleOutlinedIcon />} href="/changelog">Changelog</Button>
+                <Button color="inherit" startIcon={<QueryStatsIcon />} href="/stats">Statistik</Button>
+                <Button color="inherit" startIcon={<DescriptionIcon />} href="https://doc.pr0sauce.info">API Docs</Button>
+                <Button color="inherit" startIcon={<GitHubIcon />} href="https://github.com/pacerino">GitHub</Button>
+                {!session ? (
+                  <Button color="inherit" onClick={signIn}>
+                    Login
                   </Button>
-              )}
-            </ButtonGroup>
-          </Toolbar>
-        </AppBar>
-        <Menu
-          id="menu-appbar"
-          anchorEl={this.state.achorEL}
-          anchorOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "right",
-          }}
-          open={Boolean(this.state.anchorEL)}
-          onClose={handleClose}
-        >
-          <MenuItem
-            onClick={(e) => {
-              handleClose();
-              signOut();
+                ) : (
+                    <Button
+                      aria-controls="menu-appbar"
+                      aria-haspopup="true"
+                      onClick={toggleDropdown}
+                      color="inherit"
+                      endIcon={<AccountCircle />}
+                    >
+                      {session.user.name}
+                    </Button>
+                )}
+              </ButtonGroup>
+            </Toolbar>
+          </AppBar>
+          <Menu
+            id="menu-appbar"
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
             }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            anchorEl={dropdown}
+            open={dropdownOpen}
+            onClose={toggleDropdown}
           >
-            Logout
-          </MenuItem>
-          <MenuItem onClick={handleClose}>My account</MenuItem>
-        </Menu>
-        <Drawer anchor="left" open={this.state.drawerOpen} onClose={handleDrawerClose}>
-        <ListItem component="div" disablePadding>
-          <ListItemButton sx={{ height: 56 }}>
-            <ListItemIcon>
-              <AccountCircle sx={{color: "text.primary"}} />
-            </ListItemIcon>
-            <ListItemText
-              primary={(this.state?.session.user ? this.state.session.user.name.toUpperCase() : 'Nicht eingeloggt')}
-              primaryTypographyProps={{
-                color: 'text.primary',
-                fontWeight: 'medium',
-                variant: 'body2',
+            <MenuItem
+              onClick={(e) => {
+                toggleDropdown(e);
+                signOut();
               }}
-            />
-          </ListItemButton>
-          {!this.state?.session.user ? (
-          <Tooltip title="Einloggen">
-            <IconButton size="large" onClick={signIn}>
-              <LoginIcon sx={{color: "text.primary"}}/>
-            </IconButton>
-          </Tooltip>
-          ) : (
-            <Tooltip title="Ausloggen">
-            <IconButton size="large" onClick={signOut}>
-              <LogoutIcon sx={{color: "text.primary"}}/>
-            </IconButton>
-          </Tooltip>
-          )}
-        </ListItem>
-        <Divider sx={{ borderColor: "text.primary" }}/>
-        <List>
-          <ListItem button component="a" href="/changelog">
-            <ListItemButton>
+            >
+              Logout
+            </MenuItem>
+            <MenuItem onClick={toggleDropdown}>My account</MenuItem>
+          </Menu>
+          <Drawer anchor="left" open={drawer} onClose={toggleDrawer(false)} >
+          <ListItem component="div" disablePadding>
+            <ListItemButton sx={{ height: 56 }}>
               <ListItemIcon>
-                <ArticleOutlinedIcon sx={{color: "text.primary"}}/>
+                <AccountCircle sx={{color: "text.primary"}} />
               </ListItemIcon>
-              <ListItemText>Changelog</ListItemText>
+              <ListItemText
+                primary={(session ? session.user.name.toUpperCase() : 'Nicht eingeloggt')}
+                primaryTypographyProps={{
+                  color: 'text.primary',
+                  fontWeight: 'medium',
+                  variant: 'body2',
+                }}
+              />
             </ListItemButton>
+            {!session ? (
+            <Tooltip title="Einloggen">
+              <IconButton size="large" onClick={signIn}>
+                <LoginIcon sx={{color: "text.primary"}}/>
+              </IconButton>
+            </Tooltip>
+            ) : (
+              <Tooltip title="Ausloggen">
+              <IconButton size="large" onClick={signOut}>
+                <LogoutIcon sx={{color: "text.primary"}}/>
+              </IconButton>
+            </Tooltip>
+            )}
           </ListItem>
-          <ListItem button component="a" href="/stats">
-            <ListItemButton>
-              <ListItemIcon>
-                <QueryStatsIcon sx={{color: "text.primary"}}/>
-              </ListItemIcon>
-              <ListItemText>Statistik</ListItemText>
-            </ListItemButton>
-          </ListItem>
-          <ListItem button component="a" href="https://doc.pr0sauce.info">
-            <ListItemButton>
-              <ListItemIcon>
-                <DescriptionIcon sx={{color: "text.primary"}}/>
-              </ListItemIcon>
-              <ListItemText>API Docs</ListItemText>
-            </ListItemButton>
-          </ListItem>
-          <ListItem button component="a" href="https://github.com/pacerino">
-            <ListItemButton>
-              <ListItemIcon>
-                <GitHubIcon sx={{color: "text.primary"}}/>
-              </ListItemIcon>
-              <ListItemText>GitHub</ListItemText>
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Drawer>
-    </Box>
-    );
-  }
+          <Divider sx={{ borderColor: "text.primary" }}/>
+          <List>
+            <ListItem button component="a" href="/changelog">
+              <ListItemButton>
+                <ListItemIcon>
+                  <ArticleOutlinedIcon sx={{color: "text.primary"}}/>
+                </ListItemIcon>
+                <ListItemText>Changelog</ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem button component="a" href="/stats">
+              <ListItemButton>
+                <ListItemIcon>
+                  <QueryStatsIcon sx={{color: "text.primary"}}/>
+                </ListItemIcon>
+                <ListItemText>Statistik</ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem button component="a" href="https://doc.pr0sauce.info">
+              <ListItemButton>
+                <ListItemIcon>
+                  <DescriptionIcon sx={{color: "text.primary"}}/>
+                </ListItemIcon>
+                <ListItemText>API Docs</ListItemText>
+              </ListItemButton>
+            </ListItem>
+            <ListItem button component="a" href="https://github.com/pacerino">
+              <ListItemButton>
+                <ListItemIcon>
+                  <GitHubIcon sx={{color: "text.primary"}}/>
+                </ListItemIcon>
+                <ListItemText>GitHub</ListItemText>
+              </ListItemButton>
+            </ListItem>
+          </List>
+        </Drawer>
+      </Box>
+      );
+    }
 }
 
-export default withStyles(styles)(Navbar)
+export default Navbar
 
