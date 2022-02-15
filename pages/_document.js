a11104ac7ef8161c9321c863d024f6f4333859cd
Pac <pac@pa0x43.me>
2022-02-16 00:16:58 +0100

    Added Open Sans as Font

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,9 @@ export default class MyDocument extends Document {
       <Html lang="de">
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600;700&display=swap" rel="stylesheet" /> 
         </Head>
         <body>
           <Main />
